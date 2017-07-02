6c77ded5729b80be19e1db31a70e0df19d92d7ee
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-07-02 18:24:20 -0500

    test 3

--- wp-content/themes/angular/index.php
+++ wp-content/themes/angular/index.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php require get_template_directory()./dist/index.html; ?>
-=======
 <?php require get_template_directory()."dist/index.html"; ?>
->>>>>>> c276288ae1ccd28a3cb263e3ae2b6d100adc0819
